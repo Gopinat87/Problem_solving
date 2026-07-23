54a2ef9d59fe7cbee60ec4b9d26d699a09ccf2d3
Gopinath U <124049217+Gopinat87@users.noreply.github.com>
2026-07-23 15:39:04 +0530

    Add files via upload

---
+++ PY/check_prime.py
@@ -0,0 +1,16 @@
+# check the prime number or not
+
+def is_prime(n):
+
+    if n <= 1:
+        return "Not Prime"
+
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return "Not Prime"
+
+    return "Prime"
+
+num = int(input())
+
+print(is_prime(num))
