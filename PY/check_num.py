a365cb52f1658fd08644da5da381feeebf822b0e
Gopinath U <124049217+Gopinat87@users.noreply.github.com>
2026-07-23 15:38:50 +0530

    Add files via upload

---
+++ PY/check_num.py
@@ -0,0 +1,12 @@
+# check the missing number in list
+
+def missing_number(arr, n):
+    expected_sum = n * (n + 1) // 2
+    actual_sum = sum(arr)
+    return expected_sum - actual_sum
+
+arr = [1, 2, 4, 5]
+n = 5
+
+print(missing_number(arr, n))
+
