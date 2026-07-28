3ac975302e38f72555585fb4d20cd6b81d025f46
Gopinath U <124049217+Gopinat87@users.noreply.github.com>
2026-07-28 22:28:08 +0530

    Add files via upload

---
+++ PY/large_word.py
@@ -0,0 +1,16 @@
+def largest_word(sentence):
+
+    words = sentence.split()
+
+    longest = words[0]
+
+    for word in words:
+        if len(word) > len(longest):
+            longest = word
+
+    return longest
+
+
+sentence = "Python Java FullStack Developer"
+
+print(largest_word(sentence))
