4f867644706e53f5385331e2ffa56356e71d4fae
Gopinath U <124049217+Gopinat87@users.noreply.github.com>
2026-08-08 20:17:03 +0530

    Add files via upload

---
+++ PY/check_para.py
@@ -0,0 +1,30 @@
+def is_valid(s):
+
+    stack = []
+
+    pairs = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    }
+
+    for ch in s:
+
+        if ch in "([{":
+            stack.append(ch)
+
+        else:
+            if not stack or stack[-1] != pairs[ch]:
+                return False
+
+            stack.pop()
+
+    return len(stack) == 0
+
+
+s = "{[()]}"
+
+if is_valid(s):
+    print("Valid")
+else:
+    print("Invalid")
