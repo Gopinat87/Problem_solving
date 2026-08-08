7588b689b45f9506d0649da5351ced558e0acc00
Gopinath U <124049217+Gopinat87@users.noreply.github.com>
2026-08-08 20:18:05 +0530

    Add files via upload

--- PY/merge_arr.py
+++ PY/merge_arr.py
@@ -1,20 +1,32 @@
-def merge_arrays(arr1, arr2):
+def merge_sorted_arrays(arr1, arr2):
 
-    i = j = 0
     result = []
+
+    i = 0
+    j = 0
 
     while i < len(arr1) and j < len(arr2):
 
-        if arr1[i] < arr2[j]:
+        if arr1[i] <= arr2[j]:
             result.append(arr1[i])
             i += 1
+
         else:
             result.append(arr2[j])
             j += 1
 
-    result.extend(arr1[i:])
-    result.extend(arr2[j:])
+    while i < len(arr1):
+        result.append(arr1[i])
+        i += 1
+
+    while j < len(arr2):
+        result.append(arr2[j])
+        j += 1
 
     return result
 
-print(merge_arrays([1,3,5],[2,4,6]))
+
+arr1 = [1, 3, 5, 7]
+arr2 = [2, 4, 6, 8]
+
+print(merge_sorted_arrays(arr1, arr2))
