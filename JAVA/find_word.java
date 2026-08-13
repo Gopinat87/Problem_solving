4b143f15c6f62551dca46db695beefa3643a4a2c
Gopinath U <124049217+Gopinat87@users.noreply.github.com>
2026-08-13 09:53:19 +0530

    Add files via upload

--- JAVA/find_word.java
+++ JAVA/find_word.java
@@ -1,24 +1,56 @@
+// Find the Longest Word with Unique Characters
+// Question
+
+// Given a sentence, find the longest word that contains no repeated characters.
+
+// Input
+
+// "apple dog house banana"
+
+// Output
+
+// house
+
+// Because:
+
+// apple   → p repeats
+// dog     → unique
+// house   → unique
+// banana  → a repeats
+
+import java.util.*;
+
 public class find_word {
 
-    public static String largestWord(String sentence){
+    public static String longestUniqueWord(String sentence) {
 
         String[] words = sentence.split(" ");
 
-        String longest = words[0];
+        String longest = "";
 
-        for(String word : words){
+        for (String word : words) {
 
-            if(word.length() > longest.length())
-                longest = word;
+            HashSet<Character> set = new HashSet<>();
+
+            for (char ch : word.toCharArray()) {
+                set.add(ch);
+            }
+
+            if (set.size() == word.length()) {
+
+                if (word.length() > longest.length()) {
+                    longest = word;
+                }
+            }
         }
 
         return longest;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        String sentence = "Python Java FullStack Developer";
+        String sentence = "apple dog house banana";
 
-        System.out.println(largestWord(sentence));
+        System.out.println(longestUniqueWord(sentence));
     }
 }
